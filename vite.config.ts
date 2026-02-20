f354bdf90e70eeb3fb530a472177a9685eb9ac88
Kavenjun <y504877485@qq.com>
2026-02-20 18:38:54 +0800

    Force Cache Bust: New filenames for immediate update

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/gourmet-city/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].${Date.now()}.js`,
+        chunkFileNames: `assets/[name].${Date.now()}.js`,
+        assetFileNames: `assets/[name].${Date.now()}.[ext]`
+      }
+    }
+  }
 })
